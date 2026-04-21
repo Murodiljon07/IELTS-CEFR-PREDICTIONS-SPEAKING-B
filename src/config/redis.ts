7c7acf3eea2dc34afc92301be042fec722491b55
Murodiljon07 <murodiljonabdumutalovtg1@gmail.com>
2026-04-21 09:53:09 +0500

    database configs is now redy

---
+++ src/config/redis.ts
@@ -0,0 +1,12 @@
+import Redis from "redis";
+
+const redisClient = Redis.createClient({
+  url: process.env.REDIS_URL || "redis://localhost:6379",
+});
+
+redisClient.on("error", (err) => console.error("Redis Client Error", err));
+redisClient.on("connect", () => console.log("Redis Connected"));
+
+redisClient.connect();
+
+export default redisClient;
