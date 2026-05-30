8fc4811d0671ca0a1099016a707c75d3ebc75481
Murodiljon07 <murodiljonabdumutalovtg1@gmail.com>
2026-05-31 01:35:11 +0500

    db chesk

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ import http from "http";
 import cros from "cors";
 
 import connectDB from "./src/config/db.js";
-connectDB();
+await connectDB();
 
 import app from "./src/app.js";
 
@@ -12,6 +12,7 @@ app.use(cros());
 
 const server = http.createServer(app);
 const PORT = process.env.PORT;
+console.log("DATABASE_URL:", process.env.DATABASE_URL);
 
 server.listen(PORT, () => {
   console.log(
