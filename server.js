9e849d475fec7797707ffedb7007cff8347eda04
Murodiljon07 <murodiljonabdumutalovtg1@gmail.com>
2026-05-31 01:18:38 +0500

    add cros

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 import dotenv from "dotenv";
 dotenv.config();
 import http from "http";
+import cros from "cors";
 
 import connectDB from "./src/config/db.js";
 connectDB();
 
 import app from "./src/app.js";
+
+app.use(cros());
 
 const server = http.createServer(app);
 const PORT = process.env.PORT;
